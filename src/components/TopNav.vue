c3148b367c608655afa84ebce69d0bcddeecfa36
pras0044 <prashan@algonquincollege.com>
2024-12-12 18:59:17 -0500

    commit

--- src/components/TopNav.vue
+++ src/components/TopNav.vue
@@ -2,7 +2,7 @@
   <nav>
     <div class="logo">
       <a href="/">
-        <img src="/algonquin-pet-store.png" alt="Algonquin Pet Store Logo">
+        <img src="/bestbuyimage.png" alt="BestBuy Store Logo">
       </a>
       Admin Portal
     </div>
